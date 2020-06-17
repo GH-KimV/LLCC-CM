c2deaff62b7479fa79c15041e9a9a1bc18140d09
Kim Vo <kimlongt.vo@gmail.com>
2020-06-17 16:16:19 -0700

    almost mock perfect

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-
 const About = () => (
-    <div className='container pink'>
-    <h1>About</h1>
+    <div className='container'>
+        <div className='pink-container'>
+            <div className='text-box-shadow-teal'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+                error ad consequatur
+            </div>
+        </div>
+        <div className='teal-container'>
+            <div className='text-box-shadow-pink'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+                error ad consequatur
+            </div>
+        </div>
+        <button className="action-btn"><strong>MEET THE TEAM</strong></button>
     </div>
 );
 
