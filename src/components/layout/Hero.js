9a97a1c12b160d7c7b6e2a7505a9119d607615ac
Kim Vo <kimlongt.vo@gmail.com>
2020-06-18 11:03:55 -0700

    fixed hamburger menu and completed expectatns page

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div  className='container text-box'>
+        <>
             <h3 className='hero-header'>MAIN POINT OF THE WEEK</h3>
             <div>God gave Samson strength to defeat the Philistines.</div>
-        </div>
+        </>
     );
 };
 
