87ce1d1dff0e67047829aba444ad1e3244b6cbb0
Kim Vo <kimlongt.vo@gmail.com>
2020-06-18 11:18:13 -0700

    team page

--- src/components/pages/Team.js
+++ src/components/pages/Team.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
 
 const Team = () => {
     return (
-        <div>
-            <p>Meet The Team</p>
-        </div>
-    )
-}
+        <>
+            <div className='pink-container'>
+                <div className="text-box-shadow-teal">MEET THE TEAM!</div>
+            </div>
+            <div className="white-container">
+                <h3>PASTOR KYUHWA LEE</h3>
+                <div className="row">
+                <div className="rnd-pic-yellow">PICTURE</div>
+                <div className="p-container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ipsa laboriosam quos delectus non, officiis dicta voluptatibus sed harum</div>
+                </div>
+            </div>
+            <div className="teal-container">
+                <h3>DEACON LEE</h3>
+                <div className="row">
+                    <div className="text-box-pink">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis consectetur corporis voluptas recusandae ducimus ipsum natus eum obcaecati, architecto fugit voluptatum</div>
+                    <div className="rnd-pic-pink">PICTURE</div>
+                </div>
+            </div>
+            <div className="white-container">
+                <h3>BYNN DOKGO</h3>
+                <div className="row">
+                <div className="rnd-pic-yellow">PICTURE</div>
+                <div className="p-container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ipsa laboriosam quos delectus non, officiis dicta voluptatibus sed harum</div>
+                </div>
+            </div>
+            <div className="pink-container">
+                <h3>ANGIE CHUNG</h3>
+                <div className="row">
+                    <div className="text-box-teal">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis consectetur corporis voluptas recusandae ducimus ipsum natus eum obcaecati, architecto fugit voluptatum</div>
+                    <div className="rnd-pic-teal">PICTURE</div>
+                </div>
+            </div>
+            <div className="white-container">
+                <h3>EUNICE BAE</h3>
+                <div className="row">
+                <div className="rnd-pic-yellow">PICTURE</div>
+                <div className="p-container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ipsa laboriosam quos delectus non, officiis dicta voluptatibus sed harum</div>
+                </div>
+            </div>
+            <div className="teal-container">
+                <h3>PAUL HAN</h3>
+                <div className="row">
+                    <div className="text-box-pink">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis consectetur corporis voluptas recusandae ducimus ipsum natus eum obcaecati, architecto fugit voluptatum</div>
+                    <div className="rnd-pic-pink">PICTURE</div>
+                </div>
+            </div>
+        </>
+    );
+};
 
-export default Team
+export default Team;
