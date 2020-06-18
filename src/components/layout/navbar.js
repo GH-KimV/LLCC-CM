290cb43f1706b5e7aef3e284214c4c47db8edb1e
Joshua <meraki.dot@gmail.com>
2020-06-18 13:23:53 -0700

    menu & navbar fixes
    Visual
    - No longer transparent
    - Overlay is now white with an opacity of 0.3
    - Button changed to visually match design
    - Took out menu border
    - Navbar opacity changed from 0.9 to 1
    
    Structural changes
    - Burger.js created to house burger menu logic
    - Burger.js is a sibling to the nav element (it was a child to the nav element before)

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,43 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { slide as Menu } from 'react-burger-menu';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Burger from "./Burger";
 
 const Navbar = ({ title }) => {
-    return (
-        <nav className='navbar navbar-primary'>
-            <h1><Link to="/">{title}</Link></h1>
-            <Menu
-                isOpen={true}
-                burgerButtonClassName={'burger-btn'}
-                burgerBarClassName={'burger-bars'}
-                menuClassName={'bm-menu'}
-                crossButtonClassName={'bm-cross-button'}
-                crossClassName={'bm-cross'}
-            >
-                <a id='about' className='menu-item' href='/about'>
-                    ABOUT US
-                </a>
-                <a id='expect' className='menu-item' href='/expect'>
-                    WHAT TO EXPECT
-                </a>
-                <a id='contact' className='menu-item' href='/programs'>
-                    PROGRAMS
-                </a>
-                <a id='team' className='menu-item' href='/team'>
-                    MEET THE TEAM
-                </a>
-            </Menu>
-        </nav>
-    );
+  return (
+    <div className="navbar-container">
+      <nav className="navbar navbar-primary">
+        <h1>
+          <Link to="/">{title}</Link>
+        </h1>
+      </nav>
+      <Burger />
+    </div>
+  );
 };
 
 Navbar.defaultProps = {
-    title: 'LLCC CM',
+  title: "LLCC CM",
 };
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Navbar;
